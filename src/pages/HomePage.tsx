584ea5ce86082f816364949ac925ddf02c501435
4999vk <4999vk@gmail.com>
2025-03-29 05:38:03 +0100

    Button

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
-    return (
+  return (
+    <div>
+      <h1>Добро пожаловать в KidWay!</h1>
+      <p>Это главная страница вашего приложения.</p>
+      
       <div>
-        <h1>Добро пожаловать в KidWay!</h1>
-        <p>Это главная страница вашего приложения.</p>
+        <Link to="/login">
+          <button>Войти</button>
+        </Link>
+        <Link to="/register">
+          <button>Регистрация</button>
+        </Link>
+        <Link to="/organizations">
+          <button>Организации</button>
+        </Link>
       </div>
-    );
-  };
-  
-  export default HomePage;
-  
+    </div>
+  );
+};
+
+export default HomePage;
